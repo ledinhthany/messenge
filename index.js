10fc215e8cbce66c3f2cd184ee232cde2ffa2aab
ledinhthany <117443401+ledinhthany@users.noreply.github.com>
2023-04-18 18:00:32 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server);
+const io = new Server(server, {
+    cors: {
+        origin: "https://2508roblox.github.io"
+   
+
+    }
+});
 
 app.use(express.static(__dirname + '/public'));
 
