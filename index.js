c187d7950e5d3a3a00371a77d7f2c10c40b6f71d
ledinhthany <117443401+ledinhthany@users.noreply.github.com>
2023-04-19 18:07:10 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
   cors: {
-    origin: ["https://ledinhthany.github.io", "https://example.com"]
+    origin: ["https://ledinhthany.github.io"]
   }
 });
 
